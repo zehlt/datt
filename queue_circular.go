78fe2be020447f7c3f278f6ad7b9d23ec65d1e9e
Zehlt Dev <zehltdev@gmail.com>
2023-02-14 10:31:29 +0100

    impl other

--- queue_circular.go
+++ queue_circular.go
@@ -1,27 +1,99 @@
 package datt
 
 type QueueCircularArray[T any] struct {
+	front int
+	next  int
+	arr   []T
+	cap   int
+	len   int
 }
 
-func (q *QueueCircularArray[T]) Len() int {
-	return 0
+func NewQueueCircularArray[T any](initCap int) QueueCircularArray[T] {
+	return QueueCircularArray[T]{
+		cap: initCap,
+		arr: make([]T, initCap),
+	}
 }
 
 func (q *QueueCircularArray[T]) Enqueue(v T) {
+	if q.len == q.cap {
+		q.growComplex()
+	}
+
+	if q.next == q.cap {
+		q.next = 0
+	}
+
+	q.arr[q.next] = v
+	q.next++
+	q.len++
+}
+
+func (q *QueueCircularArray[T]) growComplex() {
+	newArr := make([]T, q.cap*2)
+
+	for i := 0; i < q.len; i++ {
+		if q.front == q.cap {
+			q.front = 0
+		}
+
+		newArr[i] = q.arr[q.front]
+		q.front++
+	}
+
+	q.arr = newArr
+	q.cap *= 2
+	q.front = 0
+	q.next = q.len
 }
 
 func (q *QueueCircularArray[T]) Dequeue() (T, bool) {
-	var d T
-	return d, false
+	var v T
+	if q.front == q.next {
+		return v, false
+	}
+
+	if q.front == q.cap {
+		q.front = 0
+	}
+
+	v = q.arr[q.front]
+	q.front++
+	q.len--
+	return v, true
 }
 
 func (q *QueueCircularArray[T]) Peek() (T, bool) {
-	var d T
-	return d, false
+	if q.front == q.next {
+		if q.len == q.cap {
+			return q.arr[q.front], true
+		}
+
+		var d T
+		return d, false
+	}
+
+	return q.arr[q.front], true
 }
 
 func (q *QueueCircularArray[T]) Do(f func(v T)) {
+	start := q.front
+
+	for i := 0; i < q.len; i++ {
+		if start == q.cap {
+			start = 0
+		}
+		f(q.arr[start])
+		start++
+	}
+}
+
+func (q *QueueCircularArray[T]) Len() int {
+	return q.len
 }
 
 func (q *QueueCircularArray[T]) Clear() {
+	q.next = 0
+	q.front = 0
+	q.len = 0
 }
--- queue_circular_test.go
+++ queue_circular_test.go
@@ -6,9 +6,282 @@ import (
 	"github.com/zehlt/datt"
 )
 
-func TestQueueCircularArray(t *testing.T) {
-	q := datt.QueueCircularArray[float32]{}
-	got := q.Len()
+func TestQueueCircularArrayLen(t *testing.T) {
+	t.Run("empty with cap of 5", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[float32](5)
+		got := q.Len()
 
-	AssertEqual(t, got, 0)
+		AssertEqual(t, got, 0)
+	})
+
+	t.Run("enqueue 1", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[float32](5)
+		q.Enqueue(26.2)
+		q.Enqueue(19.236)
+		got := q.Len()
+
+		AssertEqual(t, got, 2)
+	})
+}
+
+func TestQueueCircularArrayPeek(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[float32](5)
+		_, ok := q.Peek()
+
+		AssertEqual(t, ok, false)
+	})
+
+	t.Run("enqueue 1", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[float32](5)
+		q.Enqueue(26.2)
+		got, ok := q.Peek()
+
+		AssertEqual(t, got, 26.2)
+		AssertEqual(t, ok, true)
+	})
+
+	t.Run("enqueue 3", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[float32](5)
+		q.Enqueue(84.652)
+		q.Enqueue(26.2)
+		q.Enqueue(8.652)
+		got, ok := q.Peek()
+
+		AssertEqual(t, got, 84.652)
+		AssertEqual(t, ok, true)
+	})
+
+	t.Run("enqueue 3 and dequeue 3", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[float32](3)
+		q.Enqueue(84.652)
+		q.Enqueue(26.2)
+		q.Enqueue(8.652)
+
+		q.Dequeue()
+		q.Dequeue()
+		q.Dequeue()
+
+		q.Enqueue(6.0)
+		got, ok := q.Peek()
+
+		AssertEqual(t, ok, true)
+		AssertEqual(t, got, 6.0)
+	})
+
+	t.Run("force the cycle of the circular array", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](5)
+		q.Enqueue("a")
+		q.Dequeue()
+
+		q.Enqueue("b")
+		q.Enqueue("c")
+		q.Enqueue("d")
+		q.Enqueue("e")
+		q.Enqueue("f")
+
+		got, ok := q.Peek()
+		AssertEqual(t, got, "b")
+		AssertEqual(t, ok, true)
+		AssertEqual(t, q.Len(), 5)
+	})
+
+	t.Run("queue should grow the capacity", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](2)
+		q.Enqueue("a")
+		q.Enqueue("b")
+		q.Enqueue("c")
+		q.Enqueue("d")
+
+		got, ok := q.Peek()
+		AssertEqual(t, got, "a")
+		AssertEqual(t, ok, true)
+		AssertEqual(t, q.Len(), 4)
+	})
+
+	t.Run("dequeue to the farest", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](3)
+		q.Enqueue("a")
+		q.Enqueue("b")
+		q.Enqueue("c")
+		q.Dequeue()
+		q.Dequeue()
+		q.Dequeue()
+
+		_, ok := q.Dequeue()
+		AssertEqual(t, ok, false)
+		AssertEqual(t, q.Len(), 0)
+	})
+
+	t.Run("dequeue to the farest++", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](3)
+		q.Enqueue("a")
+		q.Enqueue("b")
+		q.Enqueue("c")
+		q.Dequeue()
+		q.Dequeue()
+		q.Dequeue()
+
+		q.Enqueue("d")
+		got, ok := q.Dequeue()
+
+		AssertEqual(t, got, "d")
+		AssertEqual(t, ok, true)
+		AssertEqual(t, q.Len(), 0)
+	})
+
+	t.Run("force grow: 1 1 [d b c] -> 0 4 [b c d e  ]", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](3)
+		q.Enqueue("a")
+		q.Enqueue("b")
+		q.Enqueue("c")
+		q.Dequeue()
+
+		q.Enqueue("d")
+		q.Enqueue("e")
+		got, ok := q.Peek()
+
+		AssertEqual(t, got, "b")
+		AssertEqual(t, ok, true)
+		AssertEqual(t, q.Len(), 4)
+	})
+
+}
+
+func TestQueueCircularArrayDequeue(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[float32](5)
+		_, ok := q.Dequeue()
+
+		AssertEqual(t, ok, false)
+	})
+
+	t.Run("1 element", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](5)
+		q.Enqueue("alice")
+		got, ok := q.Dequeue()
+
+		AssertEqual(t, ok, true)
+		AssertEqual(t, got, "alice")
+	})
+
+	t.Run("3 element", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](5)
+		q.Enqueue("john")
+		q.Enqueue("alice")
+		q.Enqueue("bob")
+		got, ok := q.Dequeue()
+
+		AssertEqual(t, ok, true)
+		AssertEqual(t, got, "john")
+		AssertEqual(t, q.Len(), 2)
+	})
+
+	t.Run("dequeue multiple time", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](5)
+		q.Enqueue("john")
+		q.Enqueue("alice")
+		q.Enqueue("bob")
+		q.Dequeue()
+		q.Dequeue()
+		got, ok := q.Dequeue()
+
+		AssertEqual(t, ok, true)
+		AssertEqual(t, got, "bob")
+		AssertEqual(t, q.Len(), 0)
+	})
+}
+
+func TestQueueCircularArray(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[string](5)
+		q.Clear()
+		_, ok := q.Dequeue()
+
+		AssertEqual(t, q.Len(), 0)
+		AssertEqual(t, ok, false)
+	})
+
+	t.Run("clear multiple elements should empty the queue", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[int](4)
+		q.Enqueue(4)
+		q.Enqueue(3)
+		q.Enqueue(1)
+		q.Enqueue(7)
+		q.Enqueue(9)
+
+		q.Clear()
+		_, ok := q.Dequeue()
+
+		AssertEqual(t, q.Len(), 0)
+		AssertEqual(t, ok, false)
+	})
+
+	t.Run("enqueue should work normally after clear", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[int](4)
+		q.Enqueue(4)
+		q.Enqueue(9)
+
+		q.Clear()
+
+		q.Enqueue(11)
+		q.Enqueue(7)
+		q.Enqueue(2)
+		got, ok := q.Peek()
+
+		AssertEqual(t, q.Len(), 3)
+		AssertEqual(t, ok, true)
+		AssertEqual(t, got, 11)
+	})
+}
+
+func TestQueueCircularArrayDo(t *testing.T) {
+	t.Run("empty queue should not iterate", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[int](4)
+
+		var iter int
+		q.Do(func(v int) {
+			iter++
+		})
+
+		AssertEqual(t, iter, 0)
+	})
+
+	t.Run("queue with elements should iterate on all", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[int](4)
+		var data [4]int = [4]int{1, 2, 3, 4}
+
+		for _, v := range data {
+			q.Enqueue(v)
+		}
+
+		var got [4]int
+		var i int
+		q.Do(func(v int) {
+			got[i] = v
+			i++
+		})
+
+		AssertEqual(t, got, data)
+	})
+
+	t.Run("queue with circle should iterate on all", func(t *testing.T) {
+		q := datt.NewQueueCircularArray[int](3)
+		var want [3]int = [3]int{2, 3, 4}
+
+		q.Enqueue(1)
+		q.Enqueue(2)
+		q.Enqueue(3)
+		q.Dequeue()
+		q.Enqueue(4)
+
+		var got [3]int
+		var i int
+		q.Do(func(v int) {
+			got[i] = v
+			i++
+		})
+
+		AssertEqual(t, got, want)
+	})
 }
